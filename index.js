5f01ccd9064d765ce522edaf77fd69901de8c5fa
Desiree Morris <desiquinncompany@gmail.com>
2019-09-05 09:15:12 -0400

    formatted delete and put endpoints

--- index.js
+++ index.js
@@ -20,6 +20,31 @@ server.get('/api/posts', (req, res) => {
 
 // POST	/api/posts	Creates a post using the information sent inside the request body.
 
+server.post('/api/posts', (req, res) => {
+    const post = req.body;
+    const title = req.body.title;
+    const contents = req.body.contents;
+
+    if(!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    db.insert(post)
+        .then((postId)=> {
+            db.findById(postId.id)
+                .then(post => {
+                    res.status(201).json(post)
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({ error: "There was an error while saving the post to the database" });
+                });
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+        });
+});
+
 
 // GET	/api/posts/:id	Returns the post object with the specified id.
 
@@ -41,8 +66,54 @@ server.get('/api/posts/:id', (req, res) => {
 
 
 // DELETE	/api/posts/:id	Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
+
+server.delete('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    db.remove(id)
+        .then(post => {
+            if(post > 0){
+            res.status(200).end();
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post could not be removed" })
+        });
+});
+
 // PUT	/api/posts/:id	Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
 
+server.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const change = req.body
+    const title = req.body.title;
+    const contents = req.body.title;
+
+    if(!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+    db.update(id, change)
+        .then((updated) => {
+            if(updated) {
+                db.findById(id)
+                    .then(post => {
+                        res.status(200).json(post);                    
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({ error: "The post information could not be modified." });
+                    });
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            };
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post information could not be modified." });
+        });
+});
 
 // GET	/api/posts/:id/comments	Returns an array of all the comment objects associated with the post with the specified id.
 
@@ -65,6 +136,38 @@ server.get('/api/posts/:id/comments', (req, res) => {
 
 // POST	/api/posts/:id/comments	Creates a comment for the post with the specified id using information sent inside of the request body.
 
+server.post('/api/posts/:id/comments', (req, res) => {
+    const comment = req.body
+    const id = req.params.id
+    
+    if(!comment.text) {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    }
+    db.insertComment(comment)
+        .then((commentId) => {
+            if(commentId) {
+                db.findById(id)
+                    .then((post) => {
+                            res.status(201).json(post)
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({ error: "There was an error while saving the comment to the database 2" })
+                    })
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error while saving the comment to the database" })
+        });
+})
+
+// When the client makes a POST request to /api/posts/:id/comments:
+// If the post with the specified id is not found:
+// return HTTP status code 404 (Not Found). in the inside/child catch????
+// return the following JSON object: { message: "The post with the specified ID does not exist." }.
 
 
 server.listen(8000, () => console.log('sever running on port 8000'));
